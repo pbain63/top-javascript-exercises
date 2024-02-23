39afe4364e0a82274cf999a66f890a829870c6ff
pbain63 <prodipbain63@gmail.com>
2024-02-23 17:15:27 +0600

    Add different type solutions for this problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,20 +1,31 @@
-//--! Doesn't work
+//freecodecamp.org/news/how-to-reverse-a-string-in-
+//javascript-in-3-different-ways-
+//--Using decrementing for loop
+const reverseString = function (word) {
+  if (word === "") return "";
+  let newString = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    newString += word[i];
+  }
+  return newString;
+};
+//--
+//--
+//Using built-in functions
 // const reverseString = function (word) {
-//   if (word === "") return "";
-//   let newString = "";
-//   for (let i = word.length - 1; i >= 0; i--) {
-//     newString += word[i];
-//     return newString;
+//   if (word === "") {
+//     return "";
+//   } else {
+//     return word.split("").reverse().join("");
 //   }
 // };
-//--!
-const reverseString = function (word) {
-  if (word === "") {
-    return "";
-  } else {
-    return word.split("").reverse().join("");
-  }
-};
+//--
+//--
+//TOP solution:
+// const reverseString = function (string) {
+//   return string.split("").reverse().join("");
+// };
 
+//--
 // Do not edit below this line
 module.exports = reverseString;
