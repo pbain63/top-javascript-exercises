e6e90420e1bf439a9492c3d91d1a0c47fecefc33
pbain63 <prodipbain63@gmail.com>
2024-02-23 16:11:49 +0600

    solved reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
-
+//--! Doesn't work
+// const reverseString = function (word) {
+//   if (word === "") return "";
+//   let newString = "";
+//   for (let i = word.length - 1; i >= 0; i--) {
+//     newString += word[i];
+//     return newString;
+//   }
+// };
+//--!
+const reverseString = function (word) {
+  if (word === "") {
+    return "";
+  } else {
+    return word.split("").reverse().join("");
+  }
 };
 
 // Do not edit below this line
